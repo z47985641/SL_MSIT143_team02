bc6b4cbabbcce038dab6cc4375c0fe1cb0be2932
z47985641 <z47985641@gmail.com>
2022-10-28 15:52:56 +0800

    可新增圖片

--- prjWebApplication1/Controllers/MemberOnlyController.cs
+++ prjWebApplication1/Controllers/MemberOnlyController.cs
@@ -1,6 +1,10 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using PJ_MSIT143_team02.Models;
+using PJ_MSIT143_team02.ViewModels;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -20,6 +24,57 @@ namespace PJ_MSIT143_team02.Controllers
         {
             return View();
         }
-        
+
+        public IActionResult MemnerRoomNew()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult MemnerRoomNew(CRoomNew NewRoom)
+        {
+            MingSuContext db = new MingSuContext();
+            Room room = new Room();
+            room.RoomName = NewRoom.RoomName;
+            room.RoomPrice = NewRoom.RoomPrice;
+            room.RoomIntrodution = NewRoom.RoomIntrodution;
+            room.MemberId = (int)HttpContext.Session.GetInt32("MemberID");
+            room.RoomstatusId = 4;
+            room.Address = NewRoom.Address;
+            room.CreateDate = DateTime.Now;
+            room.Qty = NewRoom.Qty;
+
+            Image img = new Image();
+            var ms = new MemoryStream();
+            NewRoom.img.CopyTo(ms);
+            img.Image1 = ms.ToArray();
+
+            
+            db.Rooms.Add(room);
+            db.Images.Add(img);
+            db.SaveChanges();
+            ImageReference imgRef = new ImageReference();
+            imgRef.RoomId = db.Rooms.OrderByDescending(i => i.RoomId).FirstOrDefault().RoomId + 1;
+            imgRef.ImageId = db.Images.OrderByDescending(i => i.ImageId).FirstOrDefault().ImageId ;
+
+
+            db.ImageReferences.Add(imgRef);
+            db.SaveChanges();
+
+            return RedirectToAction("TestListView","Room");
+        }
+        //public IActionResult PhotoCreateEdit(int? ItemId)
+        //{
+        //    MingSuContext db = new MingSuContext();
+        //    if (photoData.MemberImage != null)
+        //    {
+        //        using (MemoryStream ms = new MemoryStream())
+        //        {
+        //            byte[] b_photo = photoData.MemberImage;
+        //            ms.Write(b_photo);
+        //            return File(ms.ToArray(), "image/jpeg");
+        //        }
+        //    }
+        //    return new EmptyResult();
+        //}
     }
 }
---
+++ prjWebApplication1/ViewModels/CRoomNew.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PJ_MSIT143_team02.ViewModels
+{
+    public class CRoomNew
+    {
+        public int RoomId { get; set; }
+        public string RoomName { get; set; }
+        public decimal RoomPrice { get; set; }
+        public string RoomIntrodution { get; set; }
+        public int MemberId { get; set; }
+        public int RoomstatusId { get; set; }
+        public string Address { get; set; }
+        public DateTime? CreateDate { get; set; }
+        public string FimagePath { get; set; }
+        public int? Qty { get; set; }
+        public byte[] Fimage { get; set; }
+
+
+        public int ImageId { get; set; }
+        public byte[] Image1 { get; set; }
+        public string ImageCaption { get; set; }
+
+        public IFormFile img { get; set; }
+    }
+}
