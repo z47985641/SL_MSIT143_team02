b1b739700ae1723334c8db80497ebb26344d42bb
伊岑 鄭 <molly021456@gmail.com>
2022-10-30 17:32:07 +0800

    update checkout

--- prjWebApplication1/Controllers/CheckOutController.cs
+++ prjWebApplication1/Controllers/CheckOutController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using PJ_MSIT143_team02.Helpers;
 using PJ_MSIT143_team02.Models;
 using System;
@@ -38,5 +39,114 @@ namespace PJ_MSIT143_team02.Controllers
 
             return View(myOrder);
         }
+
+        // 新增訂單到資料庫
+        [HttpPost]
+        public async Task<IActionResult> CreateOrder(Order order)
+        {
+            //新增訂單到資料庫
+            if (ModelState.IsValid)
+            {
+                MingSuContext db = new MingSuContext();
+                order.OrderItem = SessionHelper.GetObjectFromJson<List<OrderItem>>(HttpContext.Session, "cart");
+
+                db.Add(order);
+                await db.SaveChangesAsync();
+                SessionHelper.Remove(HttpContext.Session, "cart");
+
+                return RedirectToAction("ReviewOrder", new { Id = order.OrderId});
+            }
+            return View();
+        }
+        // 取得當前訂單
+        public async Task<IActionResult> ReviewOrder(int? Id)
+        {
+            MingSuContext db = new MingSuContext();
+
+            if (Id == null)
+            {
+                return NotFound();
+            }
+
+            var order = await db.Orders.FirstOrDefaultAsync(m => m.OrderId == Id);
+            if (order.MemberId!= Convert.ToInt32(_userManager.GetUserId(User)))
+            {
+                return NotFound();
+            }
+            else
+            {
+                order.OrderItem = await db.OrderItem.Where(p => p.OrderId == Id).ToListAsync();
+                ViewBag.orderItems = GetOrderItems(order.OrderId);
+            }
+
+            return View(order);
+        }
+        // 付款
+        public async Task<IActionResult> Payment(int? Id, bool isSuccess)
+        {
+            MingSuContext db = new MingSuContext();
+
+            if (Id == null)
+            {
+                return NotFound();
+            }
+
+            var order = await db.Orders.FirstOrDefaultAsync(p => p.OrderId == Id);
+            if (order == null)
+            {
+                return NotFound();
+            }
+            else
+            {
+                if (isSuccess)
+                {
+                    order.OrderstatusId = 1;
+                    db.Update(order);
+                    await db.SaveChangesAsync();  //更新訂單狀態
+                }
+                return RedirectToAction("ReviewOrder", new { Id = order.OrderId });
+            }
+        }
+
+
+        // 取得商品詳細資料
+        private List<CartItem> GetOrderItems(int orderId)
+        {
+            MingSuContext db = new MingSuContext();
+
+            var OrderItems = db.OrderItem.Where(p => p.OrderId == orderId).ToList();
+
+            List<CartItem> orderItems = new List<CartItem>();
+            foreach (var orderitem in OrderItems)
+            {
+                CartItem item = new CartItem(orderitem);
+                item.room = db.Rooms.Single(x => x.RoomId == orderitem.RoomId);
+                orderItems.Add(item);
+            }
+
+            return orderItems;
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
     }
 }
