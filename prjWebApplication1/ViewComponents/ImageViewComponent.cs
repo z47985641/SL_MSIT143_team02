92cf3ba8aae6d3ea1ee8c2d915488f7fbebd09f2
eugene0328 <eugene0328@gmail.com>
2022-11-08 22:46:39 +0800

    房源說明圖片+設備的view compoment先建置好

---
+++ prjWebApplication1/ViewComponents/ImageViewComponent.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PJ_MSIT143_team02.Models;
+using System.Threading.Tasks;
+
+using System.Linq;
+
+namespace PJ_MSIT143_team02.ViewComponents
+{
+    public class ImageViewComponent : ViewComponent
+    {
+        public async Task<IViewComponentResult> InvokeAsync(int? id)
+        {
+
+            return View();
+        }
+    }
+}
