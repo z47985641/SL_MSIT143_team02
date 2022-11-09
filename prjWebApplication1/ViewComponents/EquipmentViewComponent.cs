b624f2188177f9b02fd17dacfe678fdfc27eff3e
eugene0328 <eugene0328@gmail.com>
2022-11-09 15:38:40 +0800

    ViewComponents的後端完成，圖片轉型還在研究中

--- prjWebApplication1/ViewComponents/EquipmentViewComponent.cs
+++ prjWebApplication1/ViewComponents/EquipmentViewComponent.cs
@@ -10,8 +10,16 @@ namespace PJ_MSIT143_team02.ViewComponents
     {
         public async Task<IViewComponentResult> InvokeAsync(int? id)
         {
-
-            return View();
+            var data = (from e in (new MingSuContext()).Equipment
+                        join er in (new MingSuContext()).EquipmentReferences on e.EquipmentId equals er.EquipmentId
+                        join r in (new MingSuContext()).Rooms on er.RoomId equals r.RoomId
+                        where r.RoomId == id
+                        select new Equipment
+                        {
+                            EquipmentId = e.EquipmentId,
+                            EquipmentName = e.EquipmentName,
+                        }).ToList();
+            return View(data);
         }
     }
 }
--- prjWebApplication1/ViewComponents/ImageViewComponent.cs
+++ prjWebApplication1/ViewComponents/ImageViewComponent.cs
@@ -10,8 +10,16 @@ namespace PJ_MSIT143_team02.ViewComponents
     {
         public async Task<IViewComponentResult> InvokeAsync(int? id)
         {
-
-            return View();
+            var data = (from i in (new MingSuContext()).Images
+                        join ir in (new MingSuContext()).ImageReferences on i.ImageId equals ir.ImageId
+                        join r in (new MingSuContext()).Rooms on ir.RoomId equals r.RoomId
+                        where r.RoomId == id
+                        select new Image
+                        {
+                            ImageId = i.ImageId,
+                            Image1 = i.Image1
+                        }).ToList();
+            return View(data);
         }
     }
 }
